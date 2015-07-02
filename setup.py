769c399bfd4fccefa8053eb907059bc87a1db2cd
Paul Hooijenga <paulhooijenga@gmail.com>
2015-07-02 22:24:22 +0200

    version 1.0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
         return fp.read()
 
 setup(name="gw2api",
-      version="1.0.3",
+      version="1.0.4",
       description="Guild Wars 2 API",
       author="Paul Hooijenga",
       author_email="paulhooijenga@gmail.com",
