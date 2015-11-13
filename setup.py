e0bb2d6050d6dcc667ad7da9111ba8c0bd841691
Paul Hooijenga <paulhooijenga@gmail.com>
2015-11-13 13:08:36 +0100

    version 1.0.6

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ def read(fname):
         return fp.read()
 
 setup(name="gw2api",
-      version="1.0.5",
+      version="1.0.6",
       description="Guild Wars 2 API",
       author="Paul Hooijenga",
       author_email="paulhooijenga@gmail.com",
       url="https://github.com/hackedd/gw2api",
       license="MIT",
       long_description=read("README.rst"),
-      packages=["gw2api"],
+      packages=["gw2api", "gw2api.v2"],
       install_requires=["requests"],
       classifiers=["Development Status :: 3 - Alpha",
                    "Environment :: Console",
